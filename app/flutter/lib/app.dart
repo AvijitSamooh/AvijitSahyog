7b89cac66b11bf1bcbda8b600a80e3a29f4b4a9e
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-08-28 09:38:19 +0530

    feat(flutter): persist and apply selected locale

--- app/flutter/lib/app.dart
+++ app/flutter/lib/app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import 'core/theme/app_theme.dart';
 import 'features/home/home_page.dart';
+import 'features/settings/settings_page.dart';
 import 'l10n/app_localizations.dart';
 
 class AvijitSahyogApp extends StatefulWidget {
@@ -13,17 +13,46 @@ class AvijitSahyogApp extends StatefulWidget {
 }
 
 class _AvijitSahyogAppState extends State<AvijitSahyogApp> {
+  static const _localeKey = 'selected_locale';
+
   Locale? _locale;
 
-  void setLocale(Locale locale) {
+  @override
+  void initState() {
+    super.initState();
+    _loadLocale();
+  }
+
+  Future<void> _loadLocale() async {
+    final preferences = await SharedPreferences.getInstance();
+    final languageCode = preferences.getString(_localeKey);
+
+    if (!mounted || languageCode == null) return;
+
+    final supportedCodes = AppLocalizations.supportedLocales
+        .map((locale) => locale.languageCode)
+        .toSet();
+
+    if (supportedCodes.contains(languageCode)) {
+      setState(() {
+        _locale = Locale(languageCode);
+      });
+    }
+  }
+
+  Future<void> setLocale(Locale locale) async {
     setState(() {
       _locale = locale;
     });
+
+    final preferences = await SharedPreferences.getInstance();
+    await preferences.setString(_localeKey, locale.languageCode);
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: _locale,
       onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
@@ -33,7 +62,11 @@ class _AvijitSahyogAppState extends State<AvijitSahyogApp> {
         ),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      routes: {
+        '/': (_) => HomePage(onLocaleChanged: setLocale),
+        '/settings': (_) => SettingsPage(onLocaleChanged: setLocale),
+      },
+      initialRoute: '/',
     );
   }
 }
