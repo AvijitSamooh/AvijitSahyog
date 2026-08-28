07282050ca274e22ce08311c4a0d4f1260d958d8
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-08-28 22:05:04 +0530

    feat: register donations module

--- backend/nestjs/src/app.module.ts
+++ backend/nestjs/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CausesModule } from './causes/causes.module';
+import { DonationsModule } from './donations/donations.module';
 import { OrganisationsModule } from './organisations/organisations.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, CausesModule, OrganisationsModule],
+  imports: [PrismaModule, CausesModule, OrganisationsModule, DonationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
