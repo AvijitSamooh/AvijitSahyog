9f43a4215ac8061789535aecd7cad74dbf803e06
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-08-28 21:38:53 +0530

    feat(flutter): add riverpod causes providers

---
+++ app/flutter/lib/features/causes/providers/causes_providers.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../core/network/api_client.dart';
+import '../data/causes_repository.dart';
+import '../models/cause.dart';
+
+final apiClientProvider = Provider<ApiClient>((ref) {
+  final client = ApiClient();
+  ref.onDispose(client.dispose);
+  return client;
+});
+
+final causesRepositoryProvider = Provider<CausesRepository>((ref) {
+  return CausesRepository(ref.watch(apiClientProvider));
+});
+
+final causesProvider = FutureProvider.family<List<Cause>, String>((ref, languageCode) {
+  return ref.watch(causesRepositoryProvider).getCauses(languageCode);
+});
+
+final causeProvider = FutureProvider.family<Cause, ({String slug, String languageCode})>(
+  (ref, request) {
+    return ref
+        .watch(causesRepositoryProvider)
+        .getCause(request.slug, request.languageCode);
+  },
+);
