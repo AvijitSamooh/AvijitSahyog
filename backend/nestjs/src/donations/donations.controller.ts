0f61a5c0a749a65a78ae2ca7076c78904ddd63a4
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-08-28 22:04:51 +0530

    feat: add donation endpoints

---
+++ backend/nestjs/src/donations/donations.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateDonationDto } from './dto/create-donation.dto';
+import { DonationsService } from './donations.service';
+
+@Controller('donations')
+export class DonationsController {
+  constructor(private readonly donationsService: DonationsService) {}
+
+  @Post()
+  create(@Body() input: CreateDonationDto) {
+    return this.donationsService.create(input);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.donationsService.findOne(id);
+  }
+}
