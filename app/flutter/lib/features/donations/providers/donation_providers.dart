0433615c01c433cb545a3fe653d29705db2db8fd
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-08-28 22:23:52 +0530

    fix: reuse shared API client provider

--- app/flutter/lib/features/donations/providers/donation_providers.dart
+++ app/flutter/lib/features/donations/providers/donation_providers.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/network/api_client.dart';
+import '../../causes/providers/causes_providers.dart';
 import '../data/donations_repository.dart';
 
 final donationRepositoryProvider = Provider<DonationsRepository>((ref) {
