0c74b1d43f6dba34f222bece03dfc0b003e988f3
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-08-28 20:29:36 +0530

    feat: register causes and organisations modules

--- backend/nestjs/src/app.module.ts
+++ backend/nestjs/src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CausesModule } from './causes/causes.module';
+import { OrganisationsModule } from './organisations/organisations.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CausesModule, OrganisationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
-
-
+export class AppModule {}
