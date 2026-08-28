b16dd4227e8bc4dc1e5014341125ce35a12f8a72
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-08-28 21:38:36 +0530

    feat(flutter): add organisation model

---
+++ app/flutter/lib/features/causes/models/organisation.dart
@@ -0,0 +1,52 @@
+class Organisation {
+  const Organisation({
+    required this.id,
+    required this.slug,
+    required this.name,
+    this.description,
+    this.logoUrl,
+    this.websiteUrl,
+    this.phone,
+    this.email,
+    this.address,
+    this.city,
+    this.state,
+    this.country,
+    this.latitude,
+    this.longitude,
+  });
+
+  final String id;
+  final String slug;
+  final String name;
+  final String? description;
+  final String? logoUrl;
+  final String? websiteUrl;
+  final String? phone;
+  final String? email;
+  final String? address;
+  final String? city;
+  final String? state;
+  final String? country;
+  final double? latitude;
+  final double? longitude;
+
+  factory Organisation.fromJson(Map<String, dynamic> json) {
+    return Organisation(
+      id: json['id'] as String,
+      slug: json['slug'] as String,
+      name: json['name'] as String? ?? '',
+      description: json['description'] as String?,
+      logoUrl: json['logoUrl'] as String?,
+      websiteUrl: json['websiteUrl'] as String?,
+      phone: json['phone'] as String?,
+      email: json['email'] as String?,
+      address: json['address'] as String?,
+      city: json['city'] as String?,
+      state: json['state'] as String?,
+      country: json['country'] as String?,
+      latitude: (json['latitude'] as num?)?.toDouble(),
+      longitude: (json['longitude'] as num?)?.toDouble(),
+    );
+  }
+}
