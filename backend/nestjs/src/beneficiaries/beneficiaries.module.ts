1d2e1130248179f8de06e094a629059287ae16ca
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-09-01 11:34:02 +0530

    feat(admin): register beneficiary admin controller

--- backend/nestjs/src/beneficiaries/beneficiaries.module.ts
+++ backend/nestjs/src/beneficiaries/beneficiaries.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AdminBeneficiariesController } from './admin-beneficiaries.controller';
 import { BeneficiariesController } from './beneficiaries.controller';
 import { BeneficiariesService } from './beneficiaries.service';
 
-@Module({ controllers: [BeneficiariesController], providers: [BeneficiariesService] })
+@Module({ controllers: [BeneficiariesController, AdminBeneficiariesController], providers: [BeneficiariesService] })
 export class BeneficiariesModule {}
