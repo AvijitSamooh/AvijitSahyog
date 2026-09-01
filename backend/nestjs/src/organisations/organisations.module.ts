132badb0416656232dfdf1b88f4dc0c5ffdedb25
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-09-01 10:53:19 +0530

    feat(admin): register protected organisation management routes

--- backend/nestjs/src/organisations/organisations.module.ts
+++ backend/nestjs/src/organisations/organisations.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+
+import { AuthModule } from '../auth/auth.module';
+import { AdminOrganisationsController } from './admin-organisations.controller';
 import { OrganisationsController } from './organisations.controller';
 import { OrganisationsService } from './organisations.service';
 
 @Module({
-  controllers: [OrganisationsController],
+  imports: [AuthModule],
+  controllers: [OrganisationsController, AdminOrganisationsController],
   providers: [OrganisationsService],
 })
 export class OrganisationsModule {}
