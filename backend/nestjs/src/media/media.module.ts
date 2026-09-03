d0a90c74dd0b9994fc7289f522d985b1c937f1af
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-09-04 01:06:09 +0530

    feat(media): register media controller

--- backend/nestjs/src/media/media.module.ts
+++ backend/nestjs/src/media/media.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MediaController } from './media.controller';
 import { R2StorageService } from './r2-storage.service';
 
 @Module({
+  controllers: [MediaController],
   providers: [R2StorageService],
   exports: [R2StorageService],
 })
