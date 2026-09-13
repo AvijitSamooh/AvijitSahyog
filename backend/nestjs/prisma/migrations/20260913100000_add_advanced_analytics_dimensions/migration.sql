6fe9a734f3c06a654348741999c2169031c81f25
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-09-13 13:41:31 +0530

    chore: remove advanced analytics migration from master

--- backend/nestjs/prisma/migrations/20260913100000_add_advanced_analytics_dimensions/migration.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "AnalyticsEvent"
-  ADD COLUMN "city" VARCHAR(100),
-  ADD COLUMN "language" VARCHAR(10),
-  ADD COLUMN "deviceType" VARCHAR(32);
-
-CREATE INDEX "AnalyticsEvent_city_createdAt_idx" ON "AnalyticsEvent"("city", "createdAt");
-CREATE INDEX "AnalyticsEvent_language_createdAt_idx" ON "AnalyticsEvent"("language", "createdAt");
-CREATE INDEX "AnalyticsEvent_deviceType_createdAt_idx" ON "AnalyticsEvent"("deviceType", "createdAt");
