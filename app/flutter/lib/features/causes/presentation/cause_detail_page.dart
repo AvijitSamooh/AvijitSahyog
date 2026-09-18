0786c4383f20a2786bda1e69acd2375042b143f1
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-09-18 06:40:14 +0530

    fix: restore affiliate card contact actions

--- app/flutter/lib/features/causes/presentation/cause_detail_page.dart
+++ app/flutter/lib/features/causes/presentation/cause_detail_page.dart
@@ -204,9 +204,9 @@ class _OrganisationInfoCard extends StatelessWidget {
       ),
     );
   }
-  bool _hasValidMobileNumber(String? value) {
-    final digits = (value ?? '').replaceAll(RegExp(r'\D'), '');
-    return RegExp(r'^(?:91)?[6-9]\d{9} extends StatelessWidget {
+}
+
+class _CauseErrorState extends StatelessWidget {
   const _CauseErrorState();
 
   @override
@@ -216,8 +216,9 @@ class _OrganisationInfoCard extends StatelessWidget {
           child: Text('Unable to load this cause right now.'),
         ),
       );
-}
-).hasMatch(digits);
+  bool _hasValidMobileNumber(String? value) {
+    final digits = (value ?? '').replaceAll(RegExp(r'\D'), '');
+    return RegExp(r'^(?:91)?[6-9]\d{9}$').hasMatch(digits);
   }
 
   String _whatsAppNumber(String value) {
@@ -243,16 +244,4 @@ class _OrganisationInfoCard extends StatelessWidget {
       );
     }
   }
-}
-
-class _CauseErrorState extends StatelessWidget {
-  const _CauseErrorState();
-
-  @override
-  Widget build(BuildContext context) => const Center(
-        child: Padding(
-          padding: EdgeInsets.all(24),
-          child: Text('Unable to load this cause right now.'),
-        ),
-      );
 }
