ec6b31373a56543814268d16de47e1d3edfb13c8
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-09-18 12:38:39 +0530

    fix: import shared app page scaffold

--- app/flutter/lib/features/applications/presentation/admin_applications_page.dart
+++ app/flutter/lib/features/applications/presentation/admin_applications_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../core/widgets/app_settings_menu.dart';
 import '../../../l10n/app_localizations.dart';
 import '../providers/help_applications_providers.dart';
 
