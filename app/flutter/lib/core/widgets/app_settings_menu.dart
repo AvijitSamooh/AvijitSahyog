0c93a54dec3b71d85405b3ef6f042614ea018a4c
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-09-19 20:00:29 +0530

    Fix shared scaffold navigation parameter

--- app/flutter/lib/core/widgets/app_settings_menu.dart
+++ app/flutter/lib/core/widgets/app_settings_menu.dart
@@ -141,6 +141,7 @@ class AppPageScaffold extends Scaffold {
     super.body,
     super.floatingActionButton,
     super.floatingActionButtonLocation,
+    Widget? bottomNavigationBar,
     super.bottomSheet,
     bool automaticallyImplyLeading = true,
     PreferredSizeWidget? appBarBottom,
